1310fc970d593a949d032ccd3754508dc21f6feb
VyacheslavTretyak <tretyak1c@gmail.com>
2018-07-19 10:32:03 +0300

    Add project files.

---
+++ BooksLib/Author.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BooksLib
+{
+	public class Author
+	{
+		public int ID { get; set; }
+		public string Name { get; set; }
+		[DataType(DataType.Date)]
+		[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+		public DateTime DateOfBirth { get; set; }
+		[DataType(DataType.Date)]
+		[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+		public DateTime? DateOfDeath { get; set; }
+		public override string ToString()
+		{
+			return $"{Name } ";			
+		}
+
+	}
+}
---
+++ BooksLib/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BooksLib
+{
+	public class Book
+	{
+		public int ID { get; set; }
+		public string Name { get; set; }
+		
+		public IEnumerable<Author> Author { get; set; }
+		public Publisher Publisher { get; set; }
+		[DataType(DataType.Date)]
+		[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+		public DateTime PublishDate { get; set; }
+		public int PageCount { get; set; }
+		public string ISBN { get; set; }
+	}
+}
